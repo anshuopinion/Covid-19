a97d6a798889fa1c81219dc208fead8799e00b89
anshu <anshu.shah63@gmail.com>
2020-04-27 21:50:23 +0530

    ok

--- src/components/Chart/LineChart.jsx
+++ src/components/Chart/LineChart.jsx
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
-import { Line } from "react-chartjs-2";
+import { Line, defaults } from "react-chartjs-2";
 
 import styles from "./LineChart.module.scss";
 const LineChart = () => {
+  defaults.global.tooltips.intersect = false;
+  defaults.global.tooltips.mode = "nearest";
+  defaults.global.tooltips.position = "average";
+  defaults.global.tooltips.backgroundColor = "rgba(255, 255, 255, 0.8)";
+  defaults.global.tooltips.displayColors = true;
+  defaults.global.tooltips.borderColor = "#c62828";
+  defaults.global.tooltips.borderWidth = 1;
+  defaults.global.tooltips.titleFontColor = "#000";
+  defaults.global.tooltips.bodyFontColor = "#000";
+  defaults.global.tooltips.caretPadding = 4;
+  defaults.global.tooltips.intersect = false;
+  defaults.global.tooltips.mode = "nearest";
+  defaults.global.tooltips.position = "nearest";
+  defaults.global.legend.display = true;
+  defaults.global.legend.position = "bottom";
+  defaults.global.hover.intersect = false;
+  defaults.scale.gridLines.drawOnChartArea = false;
+
   const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
@@ -15,20 +33,25 @@ const LineChart = () => {
   const lineChart = dailyData.length ? (
     <Line
       data={{
-        labels: dailyData.map(({ date }) => date),
+        labels: dailyData.map(({ date }, i) => date),
         datasets: [
           {
             data: dailyData.map(({ confirmed }) => confirmed),
-            label: "Infected",
+            borderWidth: 2,
+            borderCapStyle: "round",
+            pointBackgroundColor: "blue",
+            label: "Confirmed",
             borderColor: "blue",
-            fill: false,
+            pointHoverRadius: 2,
           },
           {
             data: dailyData.map(({ recovered }) => recovered),
+            borderWidth: 2,
+            borderCapStyle: "round",
+            pointBackgroundColor: "green",
             label: "Recovered",
             borderColor: "green",
-            backgroundColor: "rgba(0,255,0,0.5)",
-            fill: false,
+            pointHoverRadius: 2,
           },
           {
             data: dailyData.map(({ deaths }) => deaths),
@@ -57,6 +80,7 @@ const LineChart = () => {
               },
             },
           ],
+          xAxes: [],
         },
       }}
     />
